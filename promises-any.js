0f0a891bf40c4a38fe2a79bf16fc3df655ba54da
Dakshina <dakshinasingh25@gmail.com>
2023-02-07 12:42:25 +0530

    js

--- promises-any.js
+++ promises-any.js
@@ -1,19 +1,19 @@
 const p1 = new Promise((resolve, reject)=>{
     const part = 8;
-    setTimeout(() => reject("There is no such"), 7000)
+    setTimeout(() => resolve(part), 500)
 })
 
 const p2 = new Promise((resolve, reject)=>{
     const movie = 'Harry Potter';
-    setTimeout(() => resolve(movie), 500)
+    setTimeout(() => resolve(movie), 200)
 })
 
 const p3 = new Promise((resolve, reject)=>{
     const progress = 'Under Creation';
-    setTimeout(() => reject(progress), 1000)
+    setTimeout(() => resolve(progress), 1000)
 })
 
 // Promise.any([p2, p1, p3]).then(p => console.log(p));
-Promise.race([p2, p1, p3]).then(p => console.log(p));
-
+// Promise.race([p2, p1, p3]).then(p => console.log(p));
+Promise.allSettled([p2, p1, p3]).then(list => list.forEach(p=> console.log(p.value)));
 // Promise.race([p3, p2, p1]).then(p => console.log(p));
